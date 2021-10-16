f43788ba3e7c9aff57ba8a2ca0c496ce455b2572
clubcaliche <cmoreira@misfirm.com>
2021-10-16 19:10:43 -0400

    instructors - sample curl

--- instructors.sh
+++ instructors.sh
@@ -1,3 +1,3 @@
 curl -X 'GET' \
   'https://api.onepeloton.com/api/instructor?limit=60' \
-  -H 'accept: application/json' | jq -r '.data[]' #| "\(.name) - \(.username) - \(.quote)"'
+  -H 'accept: application/json' | jq -r '.data[] | "\(.name) - \(.username) - \(.quote)"'
