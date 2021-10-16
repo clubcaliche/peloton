0e970d73c5a15f128560e2a39ea13346302faaba
clubcaliche <cmoreira@misfirm.com>
2021-10-16 19:19:51 -0400

    suppress curl output

--- photos.sh
+++ photos.sh
@@ -20,7 +20,7 @@ p {
 </div>
 EOF
 
-curl -X 'GET' \
+curl -s -X 'GET' \
     'https://api.onepeloton.com/api/instructor?limit=60' \
     -H 'accept: application/json' | \
     jq -r '.data[] | "\(.image_url) \(.name)"' | \
